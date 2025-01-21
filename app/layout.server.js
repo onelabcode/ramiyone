3f277db4ac2b4acd40aff8757b19a603bd3b3408
Nftalem Arega <nftalema@zamzambank.com>
2025-01-21 04:34:27 +0300

    Update: Seo

--- app/layout.server.js
+++ app/layout.server.js
@@ -1,13 +1,3 @@
-
-export const metadata = {
-    title: "RamiyOne",
-    description: "RamiyOne scouting.",
-    icons: {
-      icon: "/biruk.png", 
-    },
-  };
-  
-  export default function ServerLayout({ children }) {
-    return <>{children}</>;
-  }
-  
+export default function ServerLayout({ children }) {
+  return <>{children}</>;
+}
